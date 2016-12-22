6606cc5d2b4fed3bc499d62d205ecad21d35ba97
Roger Stringer <roger@freekrai.net>
2016-12-22 08:30:58 -0800

    carbon

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 /*
-	our functionality app, let's you manage having elements appear on certain pages dynamically, and also looks after the most popular pages widget on home page.	
+	our functionality app, let's you manage having elements appear on certain pages dynamically, and also looks after the most popular pages widget on home page.
 */
 $( document ).ready(function() {
 	var dm = new dmBlog();
@@ -8,7 +8,7 @@ $( document ).ready(function() {
 
 function hashCode( str ){
 	if (Array.prototype.reduce){
-		return str.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+		return str.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
 	}
 	var hash = 0;
 	if (str.length === 0) return hash;
@@ -26,7 +26,7 @@ function getParameterByName(name) {
 	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-var dmBlog = function(){	
+var dmBlog = function(){
 	this.size = window.getComputedStyle(document.body,':after').getPropertyValue('content').replace(/\W/g, '');
 	this.url = document.location.pathname;
 	return this;
@@ -76,7 +76,7 @@ dmBlog.prototype.start = function(){
 	if ( this.url === "/") {
 		var page = getParameterByName("p");
 		if( page === "" || page === "1" ){
-			var popular = new mostPopular().getPages( $("#main") );
+			var popular = new mostPopular().getPages( $("#placeholder") );
 		}
 	}
 	return this;
@@ -92,7 +92,7 @@ var mostPopular = function(){
 };
 
 mostPopular.prototype.getPages = function( div_id ){
-	var _this = this;	
+	var _this = this;
 	var r={
 		headline:"Recently popular posts&hellip;",
 		clickhere:"(click to load)",
@@ -117,7 +117,7 @@ mostPopular.prototype.getPages = function( div_id ){
 					$("<a>")
 						.attr("href",item.url)
 						.attr("title",item.title)
-						.attr("data-count",item.views)						
+						.attr("data-count",item.views)
 						.text(item.title)
 				).appendTo( ul );
 			}
@@ -151,13 +151,13 @@ mostPopular.prototype.getPopular = function() {
 			clickhere: "(click to load)",
 			loading: "(loading&hellip;)"
 		};
-}();	
+}();
 */
 
 mostPopular.prototype.updatePage = function(url, title){
 //	create a unique key from the url by stripping out all non-alphanumeric characters...
 	var key = url.replace(/[\/-]/g,'');
-	
+
 	var _this = this;
 	var cnt = 0;
 	_this.flybaseRef.where({"key": key}).orderBy( {"views":-1} ).on('value').then( function( data ){
